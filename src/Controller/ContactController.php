ec9bdeb10f991f1d32a88e1f8e9d3a3d9ba651a5
Xinul999 <guichardjulien@hotmail.fr>
2025-07-03 13:09:12 +0200

    Ajout success pour Contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -7,12 +7,14 @@ use App\Helper\Helper;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request): Response
+    public function index(Request $request,  MailerInterface $mailer): Response
     {
 
         $form = $this->createForm(ContactForm::class);
@@ -25,7 +27,23 @@ final class ContactController extends AbstractController
                 $this->addFlash('error', 'Votre adresse email est invalide!');
             }
             else{
-                $this->addFlash('success', 'Votre message a été envoyé avec succès!');
+                $email = (new Email())
+                    ->from($data['email'])
+                    ->to($_ENV['CONTACT_EMAIL'])
+                    ->subject($data['sujet'])
+                    ->text(sprintf(
+                        "Message de: %s (%s)\n\n%s",
+                        $data['nom'],
+                        $data['email'],
+                        $data['message']
+                    ));
+
+                try {
+                    $mailer->send($email);
+                    $this->addFlash('success', 'Votre message a été envoyé avec succès!');
+                } catch (\Exception $e) {
+                    $this->addFlash('error', 'Erreur lors de l\'envoi du message.');
+                }
             }
 
             return $this->redirectToRoute('app_contact');
